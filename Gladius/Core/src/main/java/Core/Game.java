6658536079d294e18e4ddad2ceeccc9168288c1e
JanikSunke <janik.sunke@hotmail.dk>
2022-03-17 10:18:49 +0100

    init

---
+++ Gladius/Core/src/main/java/Core/Game.java
@@ -0,0 +1,64 @@
+package Core;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Game implements ApplicationListener {
+    private static OrthographicCamera cam;
+    private ShapeRenderer sr;
+
+    public Game(){
+        init();
+    }
+
+    public void init() {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "Gladius";
+        cfg.width = 800;
+        cfg.height = 600;
+        cfg.useGL30 = false;
+        cfg.resizable = false;
+
+        new LwjglApplication(this, cfg);
+    }
+
+    @Override
+    public void create() {
+        cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.translate(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        cam.update();
+
+        sr = new ShapeRenderer();
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(194/255f, 178/255f, 128/255f, 1); //Black = 0,0,0,1
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
