32f9a44506ef60b886139917f437f76739a4db1d
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2022-03-21 10:56:39 +0100

    Implemented classes with interfaces

---
+++ Gladius/WeaponComponent/src/main/java/Weapon/WeaponControlSystem.java
@@ -0,0 +1,12 @@
+package Weapon;
+
+import Common.data.GameData;
+import Common.data.World;
+import Common.services.IEntityProcessingService;
+
+public class WeaponControlSystem implements IEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        
+    }
+}
