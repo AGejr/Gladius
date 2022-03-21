48fba170c29992b33eab5824915369d4a0069c5b
Thomas <Raithdk@gmail.com>
2022-03-21 11:56:21 +0100

    playercontrol system finished

--- Gladius/Player/src/main/java/Player/PlayerControlSystem.java
+++ Gladius/Player/src/main/java/Player/PlayerControlSystem.java
@@ -1,5 +1,34 @@
 package Player;
 
-public class PlayerControlSystem {
+import Common.data.Entity;
+import Common.data.GameData;
+import Common.data.GameKeys;
+import Common.data.World;
+import Common.data.entityparts.LifePart;
+import Common.data.entityparts.MovingPart;
+import Common.services.IEntityProcessingService;
+
+public class PlayerControlSystem implements IEntityProcessingService {
+
+    //TODO implement attack feature
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+        for (Entity entity : world.getEntities(Player.class)){
+
+            MovingPart movingPart = entity.getPart(MovingPart.class);
+            LifePart lifePart = entity.getPart(LifePart.class);
+
+            movingPart.setLeft(gameData.getKeys().isDown(GameKeys.A));
+            movingPart.setRight(gameData.getKeys().isDown(GameKeys.D));
+            movingPart.setUp(gameData.getKeys().isDown(GameKeys.W));
+            movingPart.setDown(gameData.getKeys().isDown(GameKeys.S));
+
+            movingPart.process(gameData, entity);
+
+        }
+    }
+
 
 }
