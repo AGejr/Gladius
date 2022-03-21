9d6bdbb7f9d120b68090bf3c1c1d169e10e087a6
JanikSunke <janik.sunke@hotmail.dk>
2022-03-21 12:55:41 +0100

    Inputstream to file

--- Gladius/Core/src/main/java/Core/Game.java
+++ Gladius/Core/src/main/java/Core/Game.java
@@ -10,16 +10,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
-import javafx.scene.effect.ImageInput;
+import org.apache.commons.io.FileUtils;
 
-import javax.imageio.ImageIO;
-import javax.imageio.stream.FileImageInputStream;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
 
 public class Game implements ApplicationListener {
     private static OrthographicCamera cam;
@@ -47,49 +45,38 @@ public class Game implements ApplicationListener {
         cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         cam.update();
-        //TODO: load files properly
-        //tiledMap = new TmxMapLoader().load("C:/Users/janik/Map/Map.tmx");
-        String[] files = {"/Map/Arena_Tileset.tsx", "Map/Map.tmx"};
-        String[] newFileNames = {"Arena_Tileset.tsx", "Map.tmx"};
-        List<InputStream> streams = new ArrayList<>();
-        for (String file: files) {
-            System.out.println(file);
-            streams.add(getClass().getClassLoader().getResourceAsStream(file));
-        }
-        List<Scanner> scanners = new ArrayList<>();
-        for (InputStream stream : streams) {
-            System.out.println(stream);
-            scanners.add(new Scanner(stream));
-        }
-        File mapTmx = null;
-        for (int i = 0; i < scanners.size(); i++) {
-            File newFile = new File(newFileNames[i]);
-            try {
-                FileWriter fileWriter = new FileWriter(newFile);
-                while (scanners.get(i).hasNext()) {
-                    fileWriter.append(scanners.get(i).next());
+        String[] files = {"Map/Map.tmx", "Map/Arena_Tileset.tsx", "Map/Arena_Tileset.png"};
+
+        for (String file : files) {
+            try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(file)){
+                File newFile = new File(file);
+                if (inputStream != null) {
+                    FileUtils.copyInputStreamToFile(inputStream, newFile);
                 }
-                fileWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if (i == 0) {
-                mapTmx = newFile;
-            }
         }
-        BufferedImage BImage = null;
-        try {
-            InputStream is = new BufferedInputStream(getClass().getResourceAsStream("Map/Arena_Tileset.png"));
-            BImage = ImageIO.read(is);
-            ImageIO.write(BImage, "png", new File("Arena_Tileset.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        tiledMap = new TmxMapLoader().load(mapTmx.getPath());
+
+        tiledMap = new TmxMapLoader().load("Map/Map.tmx");
         tiledMapRenderer = new OrthoCachedTiledMapRenderer(tiledMap);
         tiledMapRenderer.setBlending(true); //Makes tiles transparent
 
         sr = new ShapeRenderer();
+    }
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+
+    private static void copyInputStreamToFile(InputStream inputStream, File file)
+            throws IOException {
+
+        // append = false
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            int read;
+            byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
     }
 
     @Override
@@ -100,7 +87,7 @@ public class Game implements ApplicationListener {
     @Override
     public void render() {
         //Gdx.gl.glClearColor(194/255f, 178/255f, 128/255f, 1); //Black = 0,0,0,1
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         cam.update();
