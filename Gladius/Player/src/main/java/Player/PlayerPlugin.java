1cb4eefa8d6709a2a058da4056b59270b5ba4804
Thomas <Raithdk@gmail.com>
2022-03-21 13:40:02 +0100

    filestream playerplugin

--- Gladius/Player/src/main/java/Player/PlayerPlugin.java
+++ Gladius/Player/src/main/java/Player/PlayerPlugin.java
@@ -4,10 +4,14 @@ import Common.data.Entity;
 import Common.data.GameData;
 import Common.data.World;
 import Common.services.IGamePluginService;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PlayerPlugin implements IGamePluginService {
 
@@ -20,12 +24,25 @@ public class PlayerPlugin implements IGamePluginService {
     }
 
     private Entity createPlayer(GameData gamedata){
+
+        String file = "GladiatorSpriteSheet.png";
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(file)){
+            File newFile = new File(file);
+            if (inputStream != null) {
+                FileUtils.copyInputStreamToFile(inputStream, newFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         File textureFile = new File("GladiatorSpriteSheet.png");
         FileHandle fileHandle = new FileHandle(textureFile);
         Texture playerTexture = new Texture(fileHandle);
         Entity player = new Player(playerTexture, 2);
-        player.setX(gamedata.getDisplayWidth()/2);
-        player.setY(gamedata.getDisplayHeight()/2);
+        player.setX(Gdx.graphics.getWidth() / 2);
+        player.setY(Gdx.graphics.getHeight() / 2);
         return player;
     }
 
