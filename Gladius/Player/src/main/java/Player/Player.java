41d01093a16c6e9d6e45e295626b8dfefd24bbab
AGejr <albert.gejr@gmail.com>
2022-03-28 15:23:27 +0200

    Added comments to player animation initialization

--- Gladius/Player/src/main/java/Player/Player.java
+++ Gladius/Player/src/main/java/Player/Player.java
@@ -29,40 +29,52 @@ public class Player extends Entity {
         if(this.getPart(AnimationPart.class) != null){
             AnimationPart animationPart = this.getPart(AnimationPart.class);
 
-            //idle animation
+            // IDLE animation
 
             Array<TextureRegion> idleRightTextures = new Array<>();
             Array<TextureRegion> idleLeftTextures = new Array<>();
+
             for (int i = 0; i < 5; i++) {
+                // IDLE right
                 idleRightTextures.add(new TextureRegion(this.getTexture(),32*i,0,32,32));
 
+                // IDLE left
                 TextureRegion idleLeftTexture = new TextureRegion(this.getTexture(),32*i,0,32,32);
                 idleLeftTexture.flip(true, false);
                 idleLeftTextures.add(idleLeftTexture);
             }
 
+            // IDLE right animation
             Animation idleRightAnimation = new Animation(0.10f,idleRightTextures);
             animationPart.addAnimation(AnimationPart.ANIMATION_STATES.IDLE_RIGHT, idleRightAnimation);
 
+            // IDLE left animation
             Animation leftIdleAnimation = new Animation(0.10f,idleLeftTextures);
             animationPart.addAnimation(AnimationPart.ANIMATION_STATES.IDLE_LEFT, leftIdleAnimation);
 
+            // Running animation
+
             Array<TextureRegion> rightMoveTextures = new Array<>();
             Array<TextureRegion> leftMoveTextures = new Array<>();
-            for (int i = 0; i < 5; i++) {
 
+            for (int i = 0; i < 5; i++) {
+                // Running right
                 rightMoveTextures.add(new TextureRegion(this.getTexture(),32*i,32,32,32));
 
+                // Running left
                 TextureRegion leftTexture = new TextureRegion(this.getTexture(),32*i,32,32,32);
                 leftTexture.flip(true,false);
-
                 leftMoveTextures.add(leftTexture);
             }
 
-            Animation leftMoveAnimation = new Animation(0.10f,leftMoveTextures);
+            // RUNNING right animation
             Animation rightMoveAnimation = new Animation(0.10f,rightMoveTextures);
-            animationPart.addAnimation(AnimationPart.ANIMATION_STATES.RUNNING_LEFT, leftMoveAnimation);
             animationPart.addAnimation(AnimationPart.ANIMATION_STATES.RUNNING_RIGHT, rightMoveAnimation);
+
+            // RUNNING left animation
+            Animation leftMoveAnimation = new Animation(0.10f,leftMoveTextures);
+            animationPart.addAnimation(AnimationPart.ANIMATION_STATES.RUNNING_LEFT, leftMoveAnimation);
+
 
 
         }
