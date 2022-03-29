9c6934dd3782cb7a6259a73387d74f3e18490631
Thomas <Raithdk@gmail.com>
2022-03-29 20:25:14 +0200

    Added animation to enemy
    Need testing for other animation than running

--- Gladius/Enemy/src/main/java/Enemy/EnemyPlugin.java
+++ Gladius/Enemy/src/main/java/Enemy/EnemyPlugin.java
@@ -3,6 +3,7 @@ package Enemy;
 import Common.data.Entity;
 import Common.data.GameData;
 import Common.data.World;
+import Common.data.entityparts.AnimationPart;
 import Common.data.entityparts.MovingPart;
 import Common.services.IGamePluginService;
 import Common.tools.FileLoader;
@@ -23,10 +24,12 @@ public class EnemyPlugin implements IGamePluginService {
 
         Entity enemy = new Enemy(file, 2);
         enemy.add(new MovingPart(30));
+        enemy.add(new AnimationPart());
         FileLoader.loadFile(file, getClass());
 
         enemy.setX(new Random().nextInt((1000 - 200) + 1) + 200);
         enemy.setY(new Random().nextInt((1000 - 400) + 1) + 400);
+
         return enemy;
     }
 
