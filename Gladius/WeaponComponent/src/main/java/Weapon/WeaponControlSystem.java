c544afcb37fe48b74943ec393eec51f1b32cc173
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2022-03-31 11:08:31 +0200

    Now it can swing the sword to both sides

--- Gladius/WeaponComponent/src/main/java/Weapon/WeaponControlSystem.java
+++ Gladius/WeaponComponent/src/main/java/Weapon/WeaponControlSystem.java
@@ -8,6 +8,7 @@ import CommonWeapon.IWeaponUser;
 import CommonWeapon.Weapon;
 import CommonWeapon.WeaponSPI;
 import CommonPlayer.Player;
+import Common.data.entityparts.AnimationPart;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class WeaponControlSystem implements IEntityProcessingService, WeaponSPI 
     private float rotationDegrees = 0.0f;
     private final float adjustX = 20.0f;
     private final float adjustY = 10.0f;
+
     @Override
     public void process(GameData gameData, World world) {
         /*
@@ -27,18 +29,35 @@ public class WeaponControlSystem implements IEntityProcessingService, WeaponSPI 
             if(entity.getTexturePath() != null) {
                 counter++;
                 for (Entity attacker: world.getEntities(Player.class)) {
+                    AnimationPart attackerAnimationPart = attacker.getPart(AnimationPart.class);
+                    if(!attackerAnimationPart.isLeft()) {
+                        float xPos = attacker.getX();
+                        float yPos = attacker.getY();
+                        entity.setX(xPos + adjustX);
+                        entity.setY(yPos + adjustY);
 
-                    float xPos = attacker.getX();
-                    float yPos = attacker.getY();
-                    entity.setX(xPos + adjustX);
-                    entity.setY(yPos + adjustY);
+                        if (counter < 15) {
+                            entity.setAngle(rotationDegrees);
+                            rotationDegrees = entity.getAngle() - 9.0f;
+                            System.out.println("RotationDegrees: " + rotationDegrees);
+                        }
 
+                    } else {
+                        if (counter == 1) {
+                            rotationDegrees = 60.0f;
+                        }
+                        float xPos = attacker.getX();
+                        float yPos = attacker.getY();
+                        entity.setX(xPos + 9);
+                        entity.setY(yPos + adjustY);
 
-                    if (counter < 15) {
-                        entity.setAngle(rotationDegrees);
-                        rotationDegrees = entity.getAngle() - 9.0f;
-                        System.out.println("RotationDegrees: " + rotationDegrees);
+                        if (counter < 15) {
+                            entity.setAngle(rotationDegrees);
+                            rotationDegrees = entity.getAngle() + 9.0f;
+                            System.out.println("RotationDegrees: " + rotationDegrees);
+                        }
                     }
+
 
                     if (counter >= 15) {
                         ((Weapon) entity).removeWeaponTexture();
@@ -46,6 +65,7 @@ public class WeaponControlSystem implements IEntityProcessingService, WeaponSPI 
                         rotationDegrees = 20.0f;
                         counter = 0;
                     }
+
                 }
             }
         }
@@ -57,12 +77,12 @@ public class WeaponControlSystem implements IEntityProcessingService, WeaponSPI 
         float xPos = attacker.getX();
         float yPos = attacker.getY();
         Weapon weaponEntity = Sword.getSword();
-        world.addEntity(weaponEntity);
         weaponEntity.setAngle(20.0f);
         rotationDegrees = weaponEntity.getAngle();
         weaponEntity.setX(xPos + adjustX);
         weaponEntity.setY(yPos + adjustY);
         weaponEntity.setWeaponTexture();
+        world.addEntity(weaponEntity);
     }
 
     //TODO change to check collision between weapon and enemy, if hit then remove HP
