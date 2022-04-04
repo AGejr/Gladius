a7e1ff1198be26b6b30d38848f597bf4591a911f
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2022-04-04 11:27:39 +0200

    Removed unnessecerry pictures

--- Gladius/WeaponComponent/src/main/java/Weapon/WeaponCollision.java
+++ Gladius/WeaponComponent/src/main/java/Weapon/WeaponCollision.java
@@ -1,4 +1,19 @@
 package Weapon;
 
-public class WeaponCollision {
+import Common.data.Entity;
+import Common.data.GameData;
+import Common.data.World;
+import Common.data.entityparts.LifePart;
+import Common.services.IPostEntityProcessingService;
+
+
+public class WeaponCollision implements IPostEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity entity: world.getEntities(Entity.class)) {
+            if (entity.getPart(LifePart.class) != null) {
+
+            }
+        }
+    }
 }
