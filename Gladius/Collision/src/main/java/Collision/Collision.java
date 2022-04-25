f62f00c5d36f07fbb3b6c4a1d48caea2848eae72
JanikSunke <janik.sunke@hotmail.dk>
2022-04-25 12:34:13 +0200

    Update Collision.java

--- Gladius/Collision/src/main/java/Collision/Collision.java
+++ Gladius/Collision/src/main/java/Collision/Collision.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class Collision implements IPostEntityProcessingService {
     private List<List<Integer>> csv;
+    private final int WALL = 1;
+    private final int WATER = 2;
 
     @Override
     public void process(GameData gameData, World world) {
@@ -24,34 +26,32 @@ public class Collision implements IPostEntityProcessingService {
                 int x = (int) (((entity.getX() + (radius * 16) / 2) / gameData.getMapWidth()) * 50); // divide by 2 to get center
 
                 MovingPart movingPart = entity.getPart(MovingPart.class);
-                // Check wall layer, if there is a wall (not 0)
-                //      Should the tile be ignored?
-                //      Is the distance between entity and tile < entity radius?
-                if ((csv.get(y - 1).get(x) != 0 && csv.get(y - 1).get(x) != 2) && (gameData.getMapHeight() - ((y - 1) * 32) - 32) - entity.getY() <= radius) {
+                // Checks around the entity if it is a wall (1) && checks if the entity's collision box top is colliding with the next tile's box
+                if ((csv.get(y - 1).get(x) == WALL) && (gameData.getMapHeight() - ((y - 1) * 32) - 32) - entity.getY() <= radius) {
                     movingPart.setColTop(true);
                 } else {
                     movingPart.setColTop(false);
                 }
-                if ((csv.get(y + 1).get(x) != 0 && csv.get(y + 1).get(x) != 2) && entity.getY() - (gameData.getMapHeight() - (y + 1) * 32) <= radius) {
+                if ((csv.get(y + 1).get(x) == WALL) && entity.getY() - (gameData.getMapHeight() - (y + 1) * 32) <= radius) {
                     movingPart.setColBot(true);
                 } else {
                     movingPart.setColBot(false);
                 }
                 // todo : radius*16/2 should be changed to texture width / 2
-                if ((csv.get(y).get(x - 1) != 0  && csv.get(y).get(x - 1) != 2) && (entity.getX() + (radius * 16) / 2) - (((x - 1) * 32) + 32) < radius) {
+                if ((csv.get(y).get(x - 1) == WALL) && (entity.getX() + (radius * 16) / 2) - (((x - 1) * 32) + 32) < radius) {
                     movingPart.setColLeft(true);
                 } else {
                     movingPart.setColLeft(false);
                 }
                 // todo : radius*16/2 should be changed to texture width / 2
-                if ((csv.get(y).get(x + 1) != 0 && csv.get(y).get(x + 1) != 2) && ((x + 1) * 32) - (entity.getX() + (radius * 16) / 2) < radius) {
+                if ((csv.get(y).get(x + 1) == WALL) && ((x + 1) * 32) - (entity.getX() + (radius * 16) / 2) < radius) {
                     movingPart.setColRight(true);
                 } else {
                     movingPart.setColRight(false);
                 }
 
-                if (csv.get(y).get(x) == 2) {
-                    movingPart.setSlow(0.5f);
+                if (csv.get(y).get(x) == WATER) {
+                    movingPart.setSlow(0.7f);
                 } else {
                     movingPart.setSlow(1f);
                 }
