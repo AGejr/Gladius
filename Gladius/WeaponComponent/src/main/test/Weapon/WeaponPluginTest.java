600247dfceff0ba9696593144d9b0bb7acd334a5
Alex Valentiner (alex123a) <alex.valentiner@hotmail.com>
2022-04-25 09:50:35 +0200

    s

---
+++ Gladius/WeaponComponent/src/main/test/Weapon/WeaponPluginTest.java
@@ -0,0 +1,26 @@
+package Weapon;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeaponPluginTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void stop() {
+    }
+}
