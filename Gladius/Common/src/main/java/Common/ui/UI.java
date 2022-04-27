77e893c87d4b47ac0b059ad2ce5dbcb4ccfd1c2a
Albert Gejr Nielsen <albert.gejr@gmail.com>
2022-04-27 12:53:49 +0200

    Merged UI element from shop branch

---
+++ Gladius/Common/src/main/java/Common/ui/UI.java
@@ -0,0 +1,33 @@
+package Common.ui;
+
+import Common.data.GameData;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+public class UI {
+
+    public static void textBox(GameData gameData, String text, float x, float y, int width, int height) {
+        ShapeRenderer shapeRenderer = new ShapeRenderer();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(x - 25, y - 15, width, height);
+        shapeRenderer.setColor(Color.DARK_GRAY);
+        shapeRenderer.rect(x - 20, y - 10, width-10, height-10);
+        shapeRenderer.end();
+
+        text(gameData, text, x, y);
+    }
+
+    private static void text(GameData gameData, String text, float x, float y) {
+        Stage stage = gameData.getStage();
+        Label.LabelStyle labelStyle = new Label.LabelStyle(new BitmapFont(), new Color(Color.WHITE));
+        Label label = new Label(text, labelStyle);
+        label.setPosition(x, y);
+        stage.addActor(label);
+        stage.act();
+        stage.draw();
+    }
+}
