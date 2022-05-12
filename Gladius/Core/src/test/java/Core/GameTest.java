2e1ca7d148f53384b4021f256c96d91770c44752
JanikSunke <janik.sunke@hotmail.dk>
2022-05-12 11:04:23 +0200

    make test

---
+++ Gladius/Core/src/test/java/Core/GameTest.java
@@ -0,0 +1,22 @@
+package Core;
+
+import org.junit.*;
+
+public class GameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void render() {
+    }
+}
