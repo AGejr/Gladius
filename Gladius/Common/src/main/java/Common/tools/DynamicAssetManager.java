e92a2b38187842af197dacdbe3ec97ea070411a0
alge <albert.gejr@gmail.com>
2022-05-13 10:36:41 +0200

    Load sounds from asset manager

--- Gladius/Common/src/main/java/Common/tools/DynamicAssetManager.java
+++ Gladius/Common/src/main/java/Common/tools/DynamicAssetManager.java
@@ -1,6 +1,7 @@
 package Common.tools;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
 import java.util.ArrayList;
@@ -9,23 +10,61 @@ public class DynamicAssetManager {
 
     public final AssetManager assetManager = new AssetManager();
 
+    private ArrayList<String> images = new ArrayList<>();
+    private ArrayList<String> sounds = new ArrayList<>();
+
     public DynamicAssetManager() {
     }
 
     public void loadAssets(){
-        FileLoader.loadFile("Minotaur.png", getClass());
-        FileLoader.loadFile("Goblin_king.png", getClass());
+        images.add("Minotaur.png");
+        images.add("Goblin_king.png");
+
+        FileLoader.loadFiles(images.toArray(new String[images.size()]), getClass());
+
+        sounds.add("Sounds/minotaur_death.mp3");
+        sounds.add("Sounds/minotaur_attack.mp3");
+        sounds.add("Sounds/goblin_death.mp3");
+        sounds.add("Sounds/goblin_attack.mp3");
+        sounds.add("Sounds/walk.mp3");
+        sounds.add("Sounds/walk_water.mp3");
+        sounds.add("Sounds/attack.mp3");
+        sounds.add("Sounds/damage.mp3");
+        sounds.add("Sounds/death.mp3");
+        sounds.add("Sounds/Explosion.mp3");
+        sounds.add("Sounds/buy.mp3");
+        sounds.add("Sounds/interact.mp3");
+        sounds.add("Sounds/wave_cleared.mp3");
+        sounds.add("Sounds/arena_entered.mp3");
+        sounds.add("Sounds/exit_arena.mp3");
+        sounds.add("Sounds/theme.ogg");
+
+        FileLoader.loadFiles(sounds.toArray(new String[sounds.size()]), getClass());
 
         loadImages();
+        loadSounds();
+
+        assetManager.finishLoading();
     }
 
     private void loadImages(){
-        assetManager.load("Minotaur.png", Texture.class);
-        assetManager.load("Goblin_king.png", Texture.class);
+        for (String image: images){
+            assetManager.load(image, Texture.class);
+        }
+    }
+
+    private void loadSounds(){
+        for (String sound: sounds){
+            assetManager.load(sound, Sound.class);
+        }
     }
 
     public Texture getTexture(String filename){
         return assetManager.get(filename, Texture.class);
+    }
+
+    public Sound getSound(String filename) {
+        return assetManager.get(filename, Sound.class);
     }
 
     public void update(){
