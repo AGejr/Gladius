187d2963fc4ce32f886ddb75d7c80060269d5b11
Thomas <Raithdk@gmail.com>
2022-05-16 14:49:45 +0200

    isCollidingTest
    need comments

--- Gladius/Collision/src/main/test/collision/collisionTest.java
+++ Gladius/Collision/src/main/test/collision/collisionTest.java
@@ -35,7 +35,7 @@ public class collisionTest {
        when(mockGameData.isGateEnabled()).thenReturn(true);
        when(mockGameData.getMapWidth()).thenReturn(1600);
        when(mockGameData.getMapHeight()).thenReturn(1280);
-       when(mockGameData.getDelta()).thenReturn(1f);
+       when(mockGameData.getDelta()).thenReturn(.1f);
 
        collision = new Collision();
        mockedWorld = mock(World.class);
@@ -91,6 +91,71 @@ public class collisionTest {
      collision.process(mockGameData,mockedWorld);
 
      assertNotEquals(testEntity.getY(),225f,1f);
+
+ }
+
+ @Test
+ public void isColliding(){
+     MovingPart part = testEntity.getPart(MovingPart.class);
+     testEntity.setY(501);
+     testEntity.setX(501);
+
+     List<List<Integer>> csv = new ArrayList<>();
+     for (int i = 1; i <= 40; i++) {
+         List<Integer> integers = new ArrayList<>();
+         for (int j = 1; j <= 50; j++) {
+             integers.add(1);
+         }
+         csv.add(integers);
+     }
+     when(mockedWorld.getCsvMap()).thenReturn(csv);
+
+     collision.process(mockGameData,mockedWorld);
+     part.process(mockGameData,testEntity);
+
+     float prevY = testEntity.getY();
+     float prevX = testEntity.getX();
+
+     part.setUp(true);
+
+     for(int i = 0; i <= 10; i++){
+         collision.process(mockGameData,mockedWorld);
+         part.process(mockGameData,testEntity);
+     }
+     assertEquals(prevY, testEntity.getY(),32f);
+     prevY = testEntity.getY();
+     part.setUp(false);
+
+     collision.process(mockGameData,mockedWorld);
+
+     part.setDown(true);
+     for(int i = 0; i <= 10; i++){
+         collision.process(mockGameData,mockedWorld);
+         part.process(mockGameData,testEntity);
+     }
+     assertEquals(prevY, testEntity.getY(),32f);
+     part.setDown(false);
+
+     collision.process(mockGameData,mockedWorld);
+
+     part.setRight(true);
+     for(int i = 0; i <= 10; i++){
+         collision.process(mockGameData,mockedWorld);
+         part.process(mockGameData,testEntity);
+     }
+     assertEquals(prevX, testEntity.getY(),32f);
+     prevX = testEntity.getY();
+     part.setRight(false);
+
+     collision.process(mockGameData,mockedWorld);
+
+     part.setLeft(true);
+     for(int i = 0; i <= 10; i++){
+         collision.process(mockGameData,mockedWorld);
+         part.process(mockGameData,testEntity);
+     }
+     assertEquals(prevX, testEntity.getY(),32f);
+     part.setLeft(false);
 
  }
 
