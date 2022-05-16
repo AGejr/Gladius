c02ba4208f83d8e94db8c917fa438899e08712d3
Thomas <Raithdk@gmail.com>
2022-05-16 14:27:51 +0200

    added nocollide test

--- Gladius/Collision/src/main/test/collision/collisionTest.java
+++ Gladius/Collision/src/main/test/collision/collisionTest.java
@@ -35,33 +35,35 @@ public class collisionTest {
        when(mockGameData.isGateEnabled()).thenReturn(true);
        when(mockGameData.getMapWidth()).thenReturn(1600);
        when(mockGameData.getMapHeight()).thenReturn(1280);
+       when(mockGameData.getDelta()).thenReturn(1f);
 
        collision = new Collision();
        mockedWorld = mock(World.class);
 
        testEntity = new Player("",2);
-       testEntity.add(new MovingPart(1));
+       testEntity.add(new MovingPart(100));
        testEntity.setY(225f);
        testEntity.setX(785f);
 
        Collection<Entity> entityList = new ArrayList<>();
        entityList.add(testEntity);
 
-       List<List<Integer>> csv = new ArrayList<>();
-       for (int i = 1; i <= 40; i++) {
-           List<Integer> integers = new ArrayList<>();
-           for (int j = 1; j <= 50; j++) {
-                   integers.add(2);
-           }
-           csv.add(integers);
-       }
-
        when(mockedWorld.getEntities()).thenReturn(entityList);
-       when(mockedWorld.getCsvMap()).thenReturn(csv);
+
    }
 
   @Test
   public void isSlowedTest(){
+
+      List<List<Integer>> csv = new ArrayList<>();
+      for (int i = 1; i <= 40; i++) {
+          List<Integer> integers = new ArrayList<>();
+          for (int j = 1; j <= 50; j++) {
+              integers.add(2);
+          }
+          csv.add(integers);
+      }
+      when(mockedWorld.getCsvMap()).thenReturn(csv);
 
       MovingPart part = testEntity.getPart(MovingPart.class);
 
@@ -74,6 +76,15 @@ public class collisionTest {
 
  @Test
  public void isGateTest(){
+     List<List<Integer>> csv = new ArrayList<>();
+     for (int i = 1; i <= 40; i++) {
+         List<Integer> integers = new ArrayList<>();
+         for (int j = 1; j <= 50; j++) {
+             integers.add(0);
+         }
+         csv.add(integers);
+     }
+     when(mockedWorld.getCsvMap()).thenReturn(csv);
 
      assertEquals(testEntity.getY(),225f,1f);
 
@@ -82,5 +93,58 @@ public class collisionTest {
      assertNotEquals(testEntity.getY(),225f,1f);
 
  }
+
+    @Test
+    public void noColliding(){
+        MovingPart part = testEntity.getPart(MovingPart.class);
+        testEntity.setY(500);
+        testEntity.setX(500);
+        part.process(mockGameData,testEntity);
+        float prevY = testEntity.getY();
+        float prevX = testEntity.getX();
+
+        List<List<Integer>> csv = new ArrayList<>();
+        for (int i = 1; i <= 40; i++) {
+            List<Integer> integers = new ArrayList<>();
+            for (int j = 1; j <= 50; j++) {
+                integers.add(0);
+            }
+            csv.add(integers);
+        }
+        when(mockedWorld.getCsvMap()).thenReturn(csv);
+
+        assertEquals(testEntity.getY(),500f,1f);
+
+        part.setUp(true);
+        part.process(mockGameData,testEntity);
+        assertTrue(prevY < testEntity.getY());
+        prevY = testEntity.getY();
+        part.setUp(false);
+
+        collision.process(mockGameData,mockedWorld);
+
+        part.setDown(true);
+        part.process(mockGameData,testEntity);
+        assertTrue(prevY > testEntity.getY());
+        part.setDown(false);
+
+        collision.process(mockGameData,mockedWorld);
+
+        part.setRight(true);
+        part.process(mockGameData,testEntity);
+        assertTrue(prevX < testEntity.getX());
+        prevX = testEntity.getX();
+        part.setRight(false);
+
+        collision.process(mockGameData,mockedWorld);
+
+        part.setLeft(true);
+        part.process(mockGameData,testEntity);
+        assertTrue(prevX > testEntity.getX());
+        part.setLeft(false);
+
+        collision.process(mockGameData,mockedWorld);
+
+    }
 
 }
